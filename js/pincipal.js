a99584b2a204564d1643f204f1ae0cc547b0fd6d
Alexandre Kawamura Tanaka <alexandretanaka@alunos.utfpr.edu.br>
2019-08-02 10:56:36 -0300

    Formulário e mexendo com Eventos (addEventListener)

--- js/pincipal.js
+++ js/pincipal.js
@@ -48,3 +48,52 @@ for(var i = 0; i < pacientes.length; i++){
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+titulo.addEventListener("click", //easterEgg
+    function() {
+        console.log("Encontrou um Easter Egg");
+    }
+);
+
+//function easterEgg(){
+//    console.log("Encontrou um Easter Egg");
+//}
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", 
+    function(event) {
+        event.preventDefault();
+        
+        var form = document.querySelector("#form-adiciona");
+
+        var nome = form.nome.value;
+        var peso = form.peso.value;
+        var altura = form.altura.value;
+        var gordura = form.gordura.value;
+
+        var trPaciente = document.createElement("tr");
+
+        var tdNome = document.createElement("td");
+        var tdPeso = document.createElement("td");
+        var tdAltura = document.createElement("td");
+        var tdGordura = document.createElement("td");
+        var tdImc = document.createElement("td");
+
+        tdNome.textContent = nome;
+        tdPeso.textContent = peso;
+        tdAltura.textContent = altura;
+        tdGordura.textContent = gordura;
+        tdImc.textContent = 0;
+
+        trPaciente.appendChild(tdNome);
+        trPaciente.appendChild(tdPeso);
+        trPaciente.appendChild(tdAltura);
+        trPaciente.appendChild(tdGordura);
+        trPaciente.appendChild(tdImc);
+
+        var tabela = document.querySelector("#tabela-pacientes");
+
+        tabela.appendChild(trPaciente);
+
+    }
+);
