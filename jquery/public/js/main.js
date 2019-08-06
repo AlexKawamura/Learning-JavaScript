9ce969a7c5b99d668b234aa765dda0cacc935980
Alexandre Kawamura Tanaka <alexandretanaka@alunos.utfpr.edu.br>
2019-08-06 10:13:33 -0300

    Contagem de caracteres e palavras

--- jquery/public/js/main.js
+++ jquery/public/js/main.js
@@ -9,3 +9,14 @@ var tamanhaFrase = $("#tamanho-frase").text(numPalavras);
             /*   = $("#tamanho-frase")
              *   = tamanhoFrase.text(numPalavras); */
 
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+    var conteudo = campo.val();
+
+    var qntPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qntPalavras);
+
+    var qntCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qntCaracteres);
+});
+
