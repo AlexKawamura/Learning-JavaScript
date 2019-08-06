0185e35e2d6422bded51aea84271969577ade62e
Alexandre Kawamura Tanaka <alexandretanaka@alunos.utfpr.edu.br>
2019-08-06 15:11:46 -0300

    Aplicando estilos Materialize e Bordas de correção

--- jquery/public/js/main.js
+++ jquery/public/js/main.js
@@ -1,6 +1,7 @@
 var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
 var botaoRestart = $("#botao-reiniciar");
+var frase = $(".frase") /* ou jQuery(".frase") */ ;
 
 /* $(function(){...}); ou $(document).ready(function(){...}); */
 
@@ -8,12 +9,11 @@ $(function() {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     botaoRestart.click(reiniciaJogo); /* .on("click", ) */
 });
 
 function atualizaTamanhoFrase() {
-    var frase = $(".frase") /* ou jQuery(".frase") */ ;
-
     var numPalavras = frase.text().split(" ").length;
     $("#tamanho-frase").text(numPalavras);
 
@@ -43,6 +43,9 @@ function inicializaCronometro() {
                 campo.attr("disabled", true);
                 clearInterval(IDcronometro);
                 botaoRestart.attr("disabled", false);
+                //campo.css("background-color", "lightgray"); Não é uma boa prática
+                //campo.addClass("campo-desativado"); //Apenas desativa
+                campo.toggleClass("campo-desativado"); // Desativa e ativa novamente
             }
         }, 1000);
         botaoRestart.attr("disabled", true);
@@ -56,4 +59,23 @@ function reiniciaJogo() {
     campo.val("");
     campo.attr("disabled", false);
     inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-verde");
+    campo.removeClass("borda-vermelha");
+}
+
+function inicializaMarcadores() {
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.text().substr(0, digitado.length);
+
+        if(digitado == comparavel) {
+           campo.addClass("borda-verde");
+           campo.removeClass("borda-vermelha");
+        } else {
+           campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+        //.startsWith(); Substituto para afunção .substr()
+    });
 }
